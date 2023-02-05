fd99fea8358588760630ccc5be673a284c20ef23
tomasoak <tomas.jpeg@gmail.com>
2023-02-05 16:38:52 +0100

    Add subflow and table_name argparse

--- week_02/ingest_data_flow.py
+++ week_02/ingest_data_flow.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+import argparse
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -28,30 +29,41 @@ def transform(df):
 
 
 @task(log_prints=True, retries=1)
-def ingest(df, user, password, host, port, db):
+def ingest(df, table_name, user, password, host, port, db):
     postgresql_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
     engine = create_engine(postgresql_url)
     engine.connect()
-    
-    table_name = 'yellow_taxi_trips'
 
     df.to_sql(name=table_name, con=engine, if_exists="append")
 
 
+@flow(name="Sub-flow", log_prints=True)
+def log_subflow(table_name: str):
+    print(f"Logging Subflow for: {table_name}")
+    
+
 @flow()
-def main_flow():
+def main_flow(params):
     user = 'root'
     password = 'root'
     host = 'localhost'
     port = '5431'
     db = 'ny_taxi'
-    
+    table_name = params.table_name
+
     url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+
+    log_subflow(table_name)
 
     raw_data = extract(url)
     data = transform(raw_data)
-    ingest(data, user, password, host, port, db)
+    ingest(data, table_name, user, password, host, port, db)
     
 
 if __name__ == "__main__":
-    main_flow()
+    parser = argparse.ArgumentParser(description='Prefect Orchestrator')
+    parser.add_argument('--table_name', help='Table for Postgres')
+
+    args = parser.parse_args()
+
+    main_flow(args)
