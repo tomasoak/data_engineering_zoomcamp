62417d27e4cd756287ce4e3ecf4d21ea2c73c190
tomasoak <tomas.jpeg@gmail.com>
2023-02-25 21:32:14 +0100

    modify model

--- week_04/dbt/models/staging/stg_yellow_tripdata.sql
+++ week_04/dbt/models/staging/stg_yellow_tripdata.sql
@@ -1,5 +1,16 @@
 {{ config(materialized='view') }}
 
-select *
-from  {{ source('staging', 'yellow_taxi_trip_partitioned') }}
-limit 10
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging', 'yellow_taxi_trip_partitoned') }}
+  where vendorid is not null 
+)
+select 
+  -- payment info
+  cast(fare_amount as numeric) as fare_amount,
+  cast(extra as numeric) as extra,
+  {{ get_payment_type_description('payment_type') }} as payment_type_description, 
+from tripdata
+where rn = 1 
